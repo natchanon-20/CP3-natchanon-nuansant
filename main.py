a67e7213483bab584399e14047c16ac095b8c3b8
natchanon-20 <masterjameza_02@hotmail.com>
2020-11-18 22:04:44 +0700

    add main file

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+สวัสดีครับ
+'''
+print(10+10*7/10)
+print("-------------------------")
+print("serenity shop Bill ")
+print("-------------------------")
+print("pen x 2    : 20 ")
+print("Ipad x 1   : 20000 ")
+print("Iphone x 1 : 15000  ")
+print("Key x 3    : 30")
+print("Remote x 1 : 150")
+print("Back pack  : 500 ")
+print("EV car toy : 5000")
+print("Game       : 2000 ")
+print("-------------------------")
+print("Total : 42700 ")
