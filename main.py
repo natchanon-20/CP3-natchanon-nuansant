aa84483d3df9694c69202a72d87d40f96abfb5d4
natchanon-20 <masterjameza_02@hotmail.com>
2020-11-19 22:24:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-'''
-สวัสดีครับ
-'''
-print(10+10*7/10)
-print("-------------------------")
-print("serenity shop Bill ")
-print("-------------------------")
-print("pen x 2    : 20 ")
-print("Ipad x 1   : 20000 ")
-print("Iphone x 1 : 15000  ")
-print("Key x 3    : 30")
-print("Remote x 1 : 150")
-print("Back pack  : 500 ")
-print("EV car toy : 5000")
-print("Game       : 2000 ")
-print("-------------------------")
-print("Total : 42700 ")
+# This is  sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
