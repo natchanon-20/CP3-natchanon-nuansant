ffbabd64801899e51b6f009b65e7dc9f46ce9ec9
natchanon-20 <masterjameza_02@hotmail.com>
2020-11-20 11:29:49 +0700

    Exercise 4

---
+++ Exercise4_natchanon_n.py
@@ -0,0 +1,19 @@
+print("Class 101   Foundation English")
+print("Class 102   General Business")
+print("Class 103   Introduction to computer Systems")
+print("Class 104   Computer Programming")
+print("..........................................")
+print("<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>")
+print("<<<<Your Score>>>>")
+Foundation = "35.6"
+print("Class 101","Foundation English","=",Foundation)
+General  = "56.8"
+print("Class 102 General Business",General)
+Introduction = 78
+print("Class 103 Introduction to computer Systems",Introduction)
+Computer = 98
+print("Class 104 Computer Programming",Computer)
+
+
+
+
