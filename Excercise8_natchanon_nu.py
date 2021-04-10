fb26f6b7358dbaa72d6cf2bdb26b8a716c0aefab
natchanon-20 <masterjameza_02@hotmail.com>
2021-04-10 22:24:42 +0700

    Continue

---
+++ Excercise8_natchanon_nu.py
@@ -0,0 +1,5 @@
+usernameInput = input("username  ")
+passwordInput = input("password  ")
+if usernameInput == "admin" and passwordInput == "1234":
+    print("welcome")
+    print("list of product")
