56d301bf47537f3eb08095b94f3c3f47a6857d80
natchanon-20 <masterjameza_02@hotmail.com>
2021-04-13 13:45:47 +0700

    done

--- Excercise8_natchanon_nu.py
+++ Excercise8_natchanon_nu.py
@@ -4,7 +4,20 @@ if usernameInput == "admin" and passwordInput == "1234":
     print("welcome")
     print("1. vat Calculator")
     print("2. product Calculator")
+    print("3. speed Calculator")
     UserSelected = int(input("number"))
     if UserSelected == 1:
         price = int(input("price THB : "))
+        vat = 7
+        result = price + (price * vat/100)
+    elif UserSelected == 2 :
+        print("price")
+        Product1 = int(input("product = "))
+        Product2 = int(input("product = "))
+        print(Product1+Product2)
+    elif UserSelected == 3 :
+        km = int(input("speed"))
+        t = 20
+        print(km / t)
+
 
